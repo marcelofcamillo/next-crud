eec764b7d64fc6439b1f8ac9bcdc0faefc862dfc
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-08-01 15:58:04 -0300

    Componente Botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from '../components/Botao';
 import Layout from '../components/Layout';
 import Tabela from '../components/Tabela';
 import Cliente from '../core/Cliente';
@@ -21,6 +22,11 @@ export default function Home() {
   return (
     <div className='flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white'>
       <Layout titulo='Cadastro Simples'>
+        <div className='flex justify-end'>
+          <Botao cor='green' className='mb-4'>
+            Novo Cliente
+          </Botao>
+        </div>
         <Tabela
           clientes={clientes}
           clienteSelecionado={clienteSelecionado}

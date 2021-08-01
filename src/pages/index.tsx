f91a2a842bbcdff6361c0f9bd2739064a4f0c0ea
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-08-01 14:59:41 -0300

    Componente Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Layout from '../components/Layout';
+import Tabela from '../components/Tabela';
+import Cliente from '../core/Cliente';
 
 export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ];
+
   return (
     <div className='flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white'>
       <Layout titulo='Cadastro Simples'>
-        <span>Conteúdo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   );

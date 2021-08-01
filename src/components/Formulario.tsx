ff4106ac51f4f6e0e0a92f22798cf1e5618c61af
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-08-01 18:08:46 -0300

    Alternando entre Tabela e Formulário

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -5,6 +5,8 @@ import Entrada from './Entrada';
 
 interface FormularioProps {
   cliente: Cliente;
+  clienteMudou?: (cliente: Cliente) => void;
+  cancelado?: () => void;
 }
 
 export default function Formulario(props: FormularioProps) {
@@ -32,10 +34,16 @@ export default function Formulario(props: FormularioProps) {
         tipo='number'
       />
       <div className='flex justify-end mt-7'>
-        <Botao cor='blue' className='mr-2'>
+        <Botao
+          cor='blue'
+          className='mr-2'
+          onClick={() => props.clienteMudou?.(new Cliente(nome, +idade, id))}
+        >
           {id ? 'Alterar' : 'Salvar'}
         </Botao>
-        <Botao cor='gray'>Cancelar</Botao>
+        <Botao cor='gray' onClick={props.cancelado}>
+          Cancelar
+        </Botao>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Botao from '../components/Botao';
 import Formulario from '../components/Formulario';
 import Layout from '../components/Layout';
@@ -20,24 +21,40 @@ export default function Home() {
     console.log(`Excluí ${cliente.nome}`);
   }
 
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente);
+  }
+
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela');
+
   return (
     <div className='flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white'>
       <Layout titulo='Cadastro Simples'>
-        <div className='flex justify-end'>
-          <Botao cor='green' className='mb-4'>
-            Novo Cliente
-          </Botao>
-        </div>
+        {visivel === 'tabela' ? (
+          <>
+            <div className='flex justify-end'>
+              <Botao
+                cor='green'
+                className='mb-4'
+                onClick={() => setVisivel('form')}
+              >
+                Novo Cliente
+              </Botao>
+            </div>
 
-        {/*
-          <Tabela
-            clientes={clientes}
-            clienteSelecionado={clienteSelecionado}
-            clienteExcluido={clienteExcluido}
+            <Tabela
+              clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            />
+          </>
+        ) : (
+          <Formulario
+            cliente={clientes[2]}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel('tabela')}
           />
-        */}
-
-        <Formulario cliente={clientes[2]} />
+        )}
       </Layout>
     </div>
   );
